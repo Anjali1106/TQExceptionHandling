7940a255ff762eab3ba59aa08d734e6f1e32bce7
Anjali <anjali123@gmail.com>
2024-11-19 19:14:35 +0530

    Add project files.

---
+++ ExceptionHandling/AgeException.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class AgeException : Exception
+    {
+        public AgeException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+    public class NameException : Exception
+    { 
+        public NameException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+
+    public class Users
+    {
+        private int age;
+        private string name;
+        public Users(int age, string name)
+        {
+            if (age >= 18)
+            {
+                this.age = age;
+
+            }
+            else
+            {
+                throw new AgeException(age + " age is not allowed");// explict call to exception using throw
+            }
+
+
+            //if(IsNullOrEmpty()) name is required
+            if (string.IsNullOrEmpty(name))
+            {
+                //this.name = name;
+                throw new NameException(name + " name is not allowed");
+
+            }
+            else {
+                //throw new NameException(name + " name is not allowed");
+                this.name = name;
+
+
+            }
+        }
+    }
+
+
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Users users = new Users(4, "");
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            try
+            {
+                Users users = new Users(5, "Anjali");
+
+            }
+            catch (NameException nameex)
+            {
+                Console.WriteLine(nameex.Message);
+
+            }
+        }
+    }
+}
